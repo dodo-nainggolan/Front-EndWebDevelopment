ec284825686ecb1546be55e982659b366c7006e6
dodo_nainggolan <dodo-nainggolan@users.noreply.github.com>
2020-04-22 16:47:45 +0700

    22042020_1647

--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -1,14 +1,32 @@
-function DataSource(onSuccess, onFailed) {
-    this.onSuccess = onSuccess;
-    this.onFailed = onFailed;
-}
-
-DataSource.prototype.searchClub = function(keyword) {
-    const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-
-    if (filteredClubs.length) {
-        this.onSuccess(filteredClubs);
-    } else {
-        this.onFailed(`${keyword} is not found`);
+//awalnya
+// function DataSource(onSuccess, onFailed) {
+//     this.onSuccess = onSuccess;
+//     this.onFailed = onFailed;
+//  }
+class DataSource {
+    constructor(onSuccess, onFailed) {
+        this.onSuccess = onSuccess;
+        this.onFailed = onFailed;
     }
-};
+
+    // awalnya
+    // DataSource.prototype.searchClub = function(keyword) {
+    //     const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+
+    //     if (filteredClubs.length) {
+    //         this.onSuccess(filteredClubs);
+    //     } else {
+    //         this.onFailed(`${keyword} is not found`);
+    //     }
+    // };
+
+    searchClub(keyword) {
+        const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+
+        if (filteredClubs.length) {
+            this.onSuccess(filteredClubs);
+        } else {
+            this.onFailed(`${keyword} is not found"`);
+        }
+    }
+}
